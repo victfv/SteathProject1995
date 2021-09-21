396dbc7368472035c1a68fb3fa01e0616572d346
Etienne <44900498+SmallMistake@users.noreply.github.com>
2021-09-20 23:55:06 -0500

    Added Reload and Sound effects to Tranq

--- Scripts/TranqGunScript.gd
+++ Scripts/TranqGunScript.gd
@@ -3,21 +3,36 @@ extends Spatial
 
 export var damage = -1
 export var speed = 50
+var maxRounds = 2
+var currentRounds = 2
+var reloading = false
 
 var bullet_scn = preload("res://Bullet.tscn")
+var shotsfx = load("res://SFX/silencedTranq.wav") 
+var reloadsfx = load("res://SFX/pistolReload.wav")
 
 func primary(state):
 	if state:
-		var new_bullet = bullet_scn.instance()
-		get_tree().get_root().add_child(new_bullet)
-		new_bullet.global_transform = $Body/SpawnPoint.global_transform
-		new_bullet.speed = speed
-		new_bullet.damage = damage
-
-#func _input(event):
-#	if Input.is_action_just_pressed("PRIMARY"):
-#		var new_bullet = bullet_scn.instance()
-#		get_tree().get_root().add_child(new_bullet)
-#		new_bullet.global_transform = $Body/SpawnPoint.global_transform
-#		new_bullet.speed = speed
-#		new_bullet.damage = damage
+		if(currentRounds > 0):
+			currentRounds -= 1
+			var new_bullet = bullet_scn.instance()
+			get_tree().get_root().add_child(new_bullet)
+			new_bullet.global_transform = $Body/SpawnPoint.global_transform
+			new_bullet.speed = speed
+			new_bullet.damage = damage
+			get_child(0).play()
+			if(currentRounds <= 0):
+				if(!reloading):
+					reload()
+			
+func reload():
+	reloading = true
+	yield(get_child(0), "finished")
+	get_child(0).stream = reloadsfx
+	get_child(0).play()
+	yield(get_child(0), "finished")
+	
+	currentRounds = maxRounds
+	reloading = false
+	get_child(0).stream = shotsfx
+	
